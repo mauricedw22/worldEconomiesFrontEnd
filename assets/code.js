e57cbeeb70104a51627f30345da7aafada018519
maurice <mauricedw22@gmail.com>
2020-07-09 10:56:27 -0400

    7th commit

--- assets/code.js
+++ assets/code.js
@@ -7,9 +7,10 @@ $(document).ready(function(){
   //feature2_ = document.getElementById("feature2");
   //feature3_ = document.getElementById("feature3");
   
-  $('#submit').on('click', function(){
+  $('#submit').on('click', function(e){
     // on clicking submit fetch values from DOM elements and use them to make request to our flask API
     var country1 = country.value;
+	// alert(country1)
     //var feature2 = feature2_.value;
     //var feature3 = feature3_.value;
     if(country1 == ""){
@@ -20,16 +21,23 @@ $(document).ready(function(){
     else{
       // replace <username> with your pythonanywhere username
       // also make sure to make changes in the url as per your flask API argument names
+	  //alert(country1)
       var requestURL = "https://mauricedw22.pythonanywhere.com/country/?country="+country1;
       console.log(requestURL); // log the requestURL for troubleshooting
-      $.getJSON(requestURL, function(data) {
-        console.log(data); // log the data for troubleshooting
-        prediction = data; //data['json_key_for_the_prediction'];
-      });
+	  $.ajax({url: requestURL, success: function(result){
+		$(".result").html(result);
+	  }});
+      /* $.getJSON(requestURL, function(data) {
+		//$.each(result, function(i, field){
+			console.log(data); // log the data for troubleshooting
+			prediction = data; //['Unemployment_Rank']; //data['json_key_for_the_prediction'];
+			//$(".result").html("Prediction is:" + prediction);
+	   //});
+      }); */
       // following lines consist of action that would be taken after the request has been read
       // for now i am just changing a <h2> tag's inner html using jquery
       // you may simple do: alert(prediction);
-      $(".result").html("Prediction is:" + prediction);
+      // $(".result").html("Prediction is:" + prediction);
     }
   });
 })
